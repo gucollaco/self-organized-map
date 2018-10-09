df0bc72cf50f0af81c089c75fc2a51b0ee10460b
gucollaco <gustavo.m.collaco@gmail.com>
2018-10-09 15:49:53 -0300

    Distances created, but is going to be modified

--- som_network.py
+++ som_network.py
@@ -19,40 +19,47 @@ def dataset():
     values = data.iloc[:, :-1]
     values = values.values
 
-    # answers
-    answers_factorized = pd.factorize(data[np.size(values,1)])[0]
-    n_answers = len(set(answers_factorized))
-    answers = np.zeros(shape=[np.size(values,0), n_answers])
-    count = 0
-    for i in answers_factorized:
-        for j  in np.unique(answers_factorized): answers[count][j] = 1 if (i==j) else 0
-        count += 1
-
     # weights matrix
-    n_neuron = int(round(np.sqrt(2) * np.size(values, 0)))
-    weights = []
+    dimension_x = dimension_y = int(np.sqrt(round(np.sqrt(2) * np.size(values, 0))))
     n_inputs = len(values[0])
     random.seed(30)
-    for i in range(n_neuron):
-        aux = [random.uniform(0, 0.1) for i in range(n_inputs)]
-        weights.append(aux)
-    weights = np.array(weights)
+    neuron_weights = np.random.uniform(low=0.05, high=0.2, size=(dimension_x, dimension_y, n_inputs))
 
     # returning values and answers
-    return values, weights, answers
+    return values, neuron_weights
 
 # competition
-#def kohonen(learning_rate=0.3, acceptable_error=0.1`):
-#    while(e > acceptable_error):
+def kohonen(values, neuron_weights, learning_rate=0.3, acceptable_error=0.1, n_epochs=300):
+    #epochs = []
+    n_inputs = len(values[0])
+    total_inputs = len(values)
+    dimension = len(neuron_weights[0])
     
+    #for epoch in n_epochs:
+    for i in range(total_inputs):
+        distances = [[] for i in range(dimension)]
+            
+        for j in range(dimension):
+            for k in range(dimension):
+                    
+                distance = 0
+                for l in range(n_inputs):
+                    distance += (neuron_weights[j][k][l] - values[i][l]) ** 2
+                
+                distances[j].append(distance)
+                
+        #epochs.append(epoch+1)
+        
+    print('distances', distances)
 
 # main function
 if __name__ == "__main__":
-
     # returning values and answers from the dataset function
-    values, weights, answers = dataset()
+    values, neuron_weights = dataset()
+    
+    #normalized_values = normalize(values)
+    kohonen(values, neuron_weights)
 
-    print('values', np.size(values, 0))
-    print('VAL', values)
-    print('answers', answers)
-    print('WEIGHTS', weights)
+    #print('VAL', values)
+    print('WEIGHTS', neuron_weights)
+    #print('KOHONEN', len(neuron_weights[0]))
